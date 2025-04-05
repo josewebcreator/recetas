a91a7f684a2bf890151bb8c9e14561ff294f4cb4
Jose Manuel <josewebcreator@gmail.com>
2025-04-05 15:55:56 -0400

    creacion del header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+//imports
+import './header.css'
+
+export default function Header(){
+
+
+
+    return(
+
+        <div className="Header">
+            <div className="logo-container">
+                <img src="default-monochrome-black.svg" alt="Logo" />
+            </div>
+        </div>
+
+    );
+
+}
