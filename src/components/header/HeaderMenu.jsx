58f59c24bc9b184762ba1f4f35f6ffdbd968e0c6
Jose Manuel <josewebcreator@gmail.com>
2025-04-06 15:40:31 -0400

    actualizacion del menu utilizacion de react icons

--- src/components/header/HeaderMenu.jsx
+++ src/components/header/HeaderMenu.jsx
@@ -1,23 +1,32 @@
 /*Imports*/
 import menuItems from './menuItems.json';
 import { Link } from 'react-router-dom';
+import { FaListUl, FaStar, FaUser } from 'react-icons/fa'; // Importa los iconos que necesitas
 
-export default function HeaderMenu(){
+//mapa de iconos
+    
+const iconMap = {
+    "recetas": FaListUl,
+    "favoritos": FaStar,
+    "Sobre Mi": FaUser,
+};
 
-
-
+export default function HeaderMenu() {
     return (
-        <nav className="HeaderMenu">
-            <ul>
-                {Object.values(menuItems).map((item, index) =>
-                    <li key={index}>
-                        <Link className='menuEnlace' to={item.url}>
-                            <img src={item.icon} alt={item.item} />
-                            <span>{item.item}</span>
-                        </Link>
-                    </li>
-                )}
-            </ul>
-        </nav>
-    )
+      <nav className="HeaderMenu">
+        <ul>
+          {Object.values(menuItems).map((item, index) => {
+            const IconComponent = iconMap[item.item]; // Obtiene el componente del icono del mapeo
+            return (
+              <li key={index}>
+                <Link className='menuEnlace' to={item.url}>
+                  {IconComponent && <IconComponent className="menuIcon" />} {/* Renderiza el icono si existe en el mapeo */}
+                  <span>{item.item}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
 }
