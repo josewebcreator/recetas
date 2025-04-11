f221d96d1aa72e3df4b150b3b69bd68336f556ab
Jose Manuel <josewebcreator@gmail.com>
2025-04-11 15:40:29 -0400

    Cambios en el context, para que almacene el estado en el navegador

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/feed/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useReducer } from 'react';
 import { MyProvider } from './context/context';
-import { reducer } from './reducer/reducer';
-import initialState from './reducer/initialState.js';
+
 
 function App() {
 
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,18 +1,46 @@
-import React, { createContext, useState, useReducer } from 'react';
+import React, { createContext, useState, useEffect, useReducer } from 'react';
 import initialState from '../reducer/initialState';
 import { reducer } from '../reducer/reducer';
 
 const MyContext = createContext();
 
 const MyProvider = ({ children }) => {
+  const localStorageKey = 'myAppContext'; // Clave para guardar en localStorage
 
-    const [state, dispatch] = useReducer(reducer, initialState);
+  // Función para cargar el estado desde localStorage
+  const loadStateFromLocalStorage = () => {
+    try {
+      const serializedState = localStorage.getItem(localStorageKey);
+      if (serializedState === null) {
+        return initialState; // No existe en localStorage, usar initialState
+      }
+      let parsedState = JSON.parse(serializedState);
+      let closeRecipe = { ...parsedState, loadedRecipe: [] };
+      return closeRecipe;
+    } catch (error) {
+      console.error("Error al cargar el estado desde localStorage:", error);
+      return initialState; // En caso de error, usar initialState
+    }
+  };
 
-    return (
-        <MyContext.Provider value={[state, dispatch]}>
-            {children}
-        </MyContext.Provider>
-    );
+  // Inicializar el estado usando useReducer y la función de carga
+  const [state, dispatch] = useReducer(reducer, loadStateFromLocalStorage());
+
+  // Efecto para guardar el estado en localStorage cada vez que cambia
+  useEffect(() => {
+    try {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem(localStorageKey, serializedState);
+    } catch (error) {
+      console.error("Error al guardar el estado en localStorage:", error);
+    }
+  }, [state]); // Dependencia en 'state' para que se ejecute al cambiar
+
+  return (
+    <MyContext.Provider value={[state, dispatch]}>
+      {children}
+    </MyContext.Provider>
+  );
 };
 
 export { MyContext, MyProvider };
